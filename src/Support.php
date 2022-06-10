99d61b9b225a75dd21b90508e8aa7d660e010915
nguyenanhung <info@nguyenanhung.com>
2022-06-10 22:29:29 +0700

    Release version 3.0.2

---
+++ src/Support.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Project pdo
+ * Created by PhpStorm
+ * User: 713uk13m <dev@nguyenanhung.com>
+ * Copyright: 713uk13m <dev@nguyenanhung.com>
+ * Date: 10/06/2022
+ * Time: 22:28
+ */
+
+namespace nguyenanhung\PDO;
+
+/**
+ * Trait Support
+ *
+ * @package   nguyenanhung\PDO
+ * @author    713uk13m <dev@nguyenanhung.com>
+ * @copyright 713uk13m <dev@nguyenanhung.com>
+ */
+trait Support
+{
+
+}
